fcf6239038bc722413291154c75c19798fb217f8
akikaaaa <772651089@qq.com>
2023-12-30 19:55:50 +0800

    update

---
+++ run_4bit_loftq.sh
@@ -0,0 +1,27 @@
+SAVE_DIR="model_zoo/loftq/"
+python quantize_save_load.py \
+    --model_name_or_path meta-llama/Llama-2-7b-hf \  # high-precision model id in HF
+    --token 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 \  # your HF token if the model is private, e.g., llama-2
+    --bits 4 \
+    --iter 5 \
+    --rank 8 \
+    --save_dir $SAVE_DIR
+
+
+# python finetune.py \
+# --base_model 'meta-llama/Llama-2-7b-hf' \
+# --data_path 'yahma/alpaca-cleaned' \
+# --output_dir './lora-alpaca' \
+# --batch_size 32 \
+# --micro_batch_size 16 \
+# --num_epochs 3 \
+# --learning_rate 1e-4 \
+# --cutoff_len 512 \
+# --val_set_size 2000 \
+# --lora_r 8 \
+# --lora_alpha 16 \
+# --lora_dropout 0.05 \
+# --lora_target_modules '[q_proj,v_proj]' \
+# --train_on_inputs \
+# --group_by_length \
+# --load_in_4bit True
---
+++ run_4bit_lora.sh
@@ -0,0 +1,17 @@
+python finetune.py \
+--base_model 'meta-llama/Llama-2-7b-hf' \
+--data_path 'yahma/alpaca-cleaned' \
+--output_dir './lora-alpaca' \
+--batch_size 32 \
+--micro_batch_size 16 \
+--num_epochs 3 \
+--learning_rate 1e-4 \
+--cutoff_len 512 \
+--val_set_size 2000 \
+--lora_r 8 \
+--lora_alpha 16 \
+--lora_dropout 0.05 \
+--lora_target_modules '[q_proj,v_proj]' \
+--train_on_inputs \
+--group_by_length \
+--load_in_4bit True
